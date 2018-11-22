3336042648ce8a75c053a4b4b14895dceb37b872
gustavons <w2zkH72HaSVq>
2018-11-22 14:56:07 -0300

    update para ionic 4 com banco firebase funcionando

---
+++ servisan/src/app/servico/servico.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ServicoPage } from './servico.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ServicoPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ServicoPage]
+})
+export class ServicoPageModule {}
