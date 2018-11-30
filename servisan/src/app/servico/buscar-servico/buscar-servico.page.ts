4d7554b57fc94bd5319c917fa5c6d4dc4f2d2c6c
gustavons <w2zkH72HaSVq>
2018-11-30 17:37:48 -0300

    parcialbusca serviço

---
+++ servisan/src/app/servico/buscar-servico/buscar-servico.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-servico',
+  templateUrl: './buscar-servico.page.html',
+  styleUrls: ['./buscar-servico.page.scss'],
+})
+export class BuscarServicoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
