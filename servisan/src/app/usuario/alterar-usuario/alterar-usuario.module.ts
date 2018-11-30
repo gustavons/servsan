eff64a3a098a9a8da421f52ca60424acea0cc3a2
Rubens Batista <rubensbatista_r17@hotmail.com>
2018-11-30 17:36:01 -0300

    parcial alterar usuario

---
+++ servisan/src/app/usuario/alterar-usuario/alterar-usuario.module.ts
@@ -0,0 +1,25 @@
+import { AlterarUsuarioPage } from './alterar-usuario.page';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AlterarUsuarioPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [AlterarUsuarioPage]
+})
+export class CadastroUsuarioPageModule {}
