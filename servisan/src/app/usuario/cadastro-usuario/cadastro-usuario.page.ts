ec28ec93c1d0e317587aebb8f6a344a390b1b7e9
Rubens Batista <rubensbatista_r17@hotmail.com>
2018-11-30 11:39:39 -0300

    gravando cadastro banco

--- servisan/src/app/usuario/cadastro-usuario/cadastro-usuario.page.ts
+++ servisan/src/app/usuario/cadastro-usuario/cadastro-usuario.page.ts
@@ -1,3 +1,7 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { Cad, CadService } from './../services/user.service';
+import { NavController, LoadingController, ModalController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroUsuarioPage implements OnInit {
 
-  constructor() { }
+  cad: Cad = {
 
-  ngOnInit() {
+  Nome: '',
+  CPF: '',  
+  createdAt: new Date().getTime(),
+}; 
+
+cadId = null;
+
+constructor(private route: ActivatedRoute, private nav: NavController, private cadService: CadService, private loadingController: LoadingController) { }
+
+ngOnInit() {
+  this.cadId = this.route.snapshot.params['id'];
+  if (this.cadId)  {
+    this.loadCad();
   }
+}
 
+async loadCad() {
+  const loading = await this.loadingController.create({
+    message: 'Carregando Cadastro..'
+  });
+  await loading.present();
+
+  this.cadService.getCad(this.cadId).subscribe(res => {
+    loading.dismiss();
+    this.cad = res;
+  });
+}
+
+async saveCad() {
+
+  const loading = await this.loadingController.create({
+    message: 'Salvando Cadastro...'
+  });
+  await loading.present();
+
+  if (this.cadId) {
+    this.cadService.updateCad(this.cad, this.cadId).then(() => {
+      loading.dismiss();
+      this.nav.navigateBack("\home", true);
+    });
+  } else {
+    this.cadService.addCad(this.cad).then(() => {
+      loading.dismiss();
+      this.nav.navigateBack("\home", true);
+    });
+  }
+}
 }
